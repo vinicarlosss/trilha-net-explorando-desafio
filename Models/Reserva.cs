824f876977a3e3eba57ea060e093794be885733b
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2025-08-25 08:37:50 -0300

    feat: completing method CadastrarHospedes

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            int numeroHospedes = hospedes.Count();
+            int capacidade = Suite.Capacidade;
+            if (numeroHospedes <= capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException("Número de hospedes superior a capacidade permitida!");
             }
         }
 
